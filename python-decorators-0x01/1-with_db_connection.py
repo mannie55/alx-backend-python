b7da3992e1783f223a9f7f101bdfb41c53db9679
nnamdi <mannnie55@gmail.com>
2025-05-20 14:39:31 +0100

    refactor log_queries decorator to improve performance and reduce overhead

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,29 @@
+import sqlite3 
+import functools
+from datetime import datetime
+
+def with_db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('my_database.db')
+        # run the function with the connection
+        result = func(conn, *args, **kwargs)
+        conn.close()
+        return result
+    return wrapper
+
+
+
+
+
+
+
+
+@with_db_connection 
+def get_user_by_id(conn, user_id): 
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,)) 
+    return cursor.fetchone() 
+#### Fetch user by ID with automatic connection handling 
+
+user = get_user_by_id(user_id=1)
+print(user)
