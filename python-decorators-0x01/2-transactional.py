e8bd7f97ae6bb94bf2d5b4115ace5eb903652fce
nnamdi <mannnie55@gmail.com>
2025-05-20 16:31:12 +0100

    update my_database.db to reflect recent changes in data structure and content

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,41 @@
+import sqlite3 
+import functools
+
+"""your code goes here"""
+
+def with_db_connection(func):
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('my_database.db')
+        print("conn", conn)
+        result = func(conn, *args, **kwargs)
+        conn.close()
+        return result
+    return wrapper
+    
+
+def transactional(func):
+    def wrapper(*args, **kwargs):
+        try:
+            conn = args[0]
+            conn.execute('BEGIN')
+            result = func(*args, **kwargs)
+            conn.commit()
+            print("result", result)
+            return result
+        except Exception as e:
+            conn.rollback()
+            print("Rollback!. Error in transaction", e)
+            raise e
+    return wrapper
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+    print(f"Rows affected: {cursor.rowcount}")
+    print("Email updated.")
+    return cursor.rowcount
+#### Update user's email with automatic transaction handling 
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
