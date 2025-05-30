dec8ffafb2bce91c2d2f708331ff05a196c8bb02
nnamdi <mannnie55@gmail.com>
2025-05-30 23:23:50 +0100

    fix: add missing path for API authentication in URL configuration

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
+     path('api-auth/', include('rest_framework.urls')),  
 ]
