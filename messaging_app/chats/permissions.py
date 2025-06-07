b6f819730f08995bfcc57892da004dd0dbc117a0
nnamdi <mannnie55@gmail.com>
2025-06-07 15:44:38 +0100

    refactor: improve permission checks for conversation participation

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -6,13 +6,20 @@ class IsParticipantOfConversation(permissions.BasePermission):
     allow only authenticated users
     allow access only if the user is a participant of the conversation.
     """
+    
+    SAFE_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+
     def has_permission(self, request, view):
         # Check if the user is authenticated
         return request.user and request.user.is_authenticated
     
-    
+
     def has_object_permission(self, request, view, obj):
         # Check if the user is a participant in the conversation
+        if request.method not in permissions.SAFE_METHODS:
+            return False
+
         user = request.user
         if hasattr(obj, 'participants'):
             return user in obj.participants.all()
